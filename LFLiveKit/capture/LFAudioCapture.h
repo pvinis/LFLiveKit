b1c3947e79e1a11bfd7fabac013c32d6b288e1d8
Pavlos Vinieratos <pvinis@gmail.com>
2017-03-03 17:44:20 +0100

    cleanup audio a bit

--- LFLiveKit/capture/LFAudioCapture.h
+++ LFLiveKit/capture/LFAudioCapture.h
@@ -10,44 +10,29 @@
 #import <AVFoundation/AVFoundation.h>
 #import "LFLiveAudioConfiguration.h"
 
-#pragma mark -- AudioCaptureNotification
-/** compoentFialed will post the notification */
+@class LFAudioCapture;
+
 extern NSString *_Nullable const LFAudioComponentFailedToCreateNotification;
 
-@class LFAudioCapture;
-/** LFAudioCapture callback audioData */
+
 @protocol LFAudioCaptureDelegate <NSObject>
+
 - (void)captureOutput:(nullable LFAudioCapture *)capture audioData:(nullable NSData*)audioData;
+
 @end
 
 
 @interface LFAudioCapture : NSObject
 
-#pragma mark - Attribute
-///=============================================================================
-/// @name Attribute
-///=============================================================================
-
-/** The delegate of the capture. captureData callback */
 @property (nullable, nonatomic, weak) id<LFAudioCaptureDelegate> delegate;
 
-/** The muted control callbackAudioData,muted will memset 0.*/
 @property (nonatomic, assign) BOOL muted;
 
-/** The running control start capture or stop capture*/
 @property (nonatomic, assign) BOOL running;
 
-#pragma mark - Initializer
-///=============================================================================
-/// @name Initializer
-///=============================================================================
 - (nullable instancetype)init UNAVAILABLE_ATTRIBUTE;
 + (nullable instancetype)new UNAVAILABLE_ATTRIBUTE;
 
-/**
-   The designated initializer. Multiple instances with the same configuration will make the
-   capture unstable.
- */
 - (nullable instancetype)initWithAudioConfiguration:(nullable LFLiveAudioConfiguration *)configuration NS_DESIGNATED_INITIALIZER;
 
 @end
